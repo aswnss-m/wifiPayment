17d05067169afa90b6c0f1eee7905613cb7abe56
aswnss-m <aswnss.m@gmail.com>
2023-05-07 10:01:58 +0530

    navbar styling done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "../assets/Navbar.css"
+function Navbar() {
+  return (
+    <div className="navbarContainer">
+        <nav>
+            <div className="navbarLogo">Niaz Hostel</div>
+            <div className="navbarLogin bold-text">Login</div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
